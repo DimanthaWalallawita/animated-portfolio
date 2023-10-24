3752ab2674f9b4bf016ca8235d7c1b8fdceca37e
DimanthaWalallawita <walallawitad@gmail.com>
2023-10-23 22:50:44 -0700

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import Test from "./Test";
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div id="Homepage">
+    <section>
+      <Navbar/>
+    </section>
+    <section id="Parallax">Parallax</section>
+    <section id="Services">Services</section>
+    <section id="Portfolio">Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section id="Contact">Contact</section>
+
+  {/*<Test/>
+  <Test/>*/}
+  </div>;
 };
 
 export default App;
